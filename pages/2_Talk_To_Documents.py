795d828caf64b3cdbb38b274669592c1908eb9b0
huruizverastegui <118810504+huruizverastegui@users.noreply.github.com>
2024-06-17 14:15:32 +0700

    Update 2_Talk_To_Documents.py

--- pages/2_Talk_To_Documents.py
+++ pages/2_Talk_To_Documents.py
@@ -15,6 +15,7 @@ from llama_index.core.extractors import (
     TitleExtractor,
     QuestionsAnsweredExtractor,
 )
+from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.node_parser import TokenTextSplitter
 from helpers.azhelpers import upload_to_azure_storage, list_all_containers, list_all_files
 
